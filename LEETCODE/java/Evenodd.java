517af620272f16bd4c5a12528363e38e4e0daa96
QueenColly <colleteumeh0@gmail.com>
2026-05-03 22:59:45 +0100

    first commit

---
+++ LEETCODE/java/Evenodd.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Evenodd{
+public static void main(String[] args){
+
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter an integer: ");
+    int integer = input.nextInt();
+
+         if(integer % 2 == 0){
+          System.out.println(true);
+       }
+       else{
+              System.out.println(false);
+       }
+
+    } 
+
+        public static boolean isPrime
+}
